4c1e92cbaaed227e9cae1ce2d08226c663dfd390
AthmanDodoe <jilloathman943@gmail.com>
2023-02-02 22:39:36 +0300

    2. Basic annotations - floor

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -2,6 +2,6 @@
 """ Returns the floor of the float """
 
 
-def floor(n: float) -> float:
+def floor(n: float) -> int:
     """ takes a float n and returns the floor of the float """
     return int(n)
